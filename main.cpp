ad9d7649be1c86b9d38b016816a1ec33e388391f
WaterLin <liumangxuxu@126.com>
2016-05-31 18:43:31 +0800

    add init file

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <GL/freeglut.h>
+
+float angle = 0.0f;
+
+void changeSize(int w, int h) {
+
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window of zero width).
+	if (h == 0)
+		h = 1;
+	float ratio = 1.0* w / h;
+
+	// Use the Projection Matrix
+	glMatrixMode(GL_PROJECTION);
+
+	// Reset Matrix
+	glLoadIdentity();
+
+	// Set the viewport to be the entire window
+	glViewport(0, 0, w, h);
+
+	// Set the correct perspective.
+	gluPerspective(45, ratio, 1, 1000);
+
+	// Get Back to the Modelview
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void renderScene(void) {
+
+	// Clear Color and Depth Buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Reset transformations
+	glLoadIdentity();
+	// Set the camera
+	gluLookAt(0.0f, 0.0f, 10.0f,
+		0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f);
+
+	glRotatef(angle, 0.0f, 1.0f, 0.0f);
+
+	glBegin(GL_TRIANGLES);
+	glVertex3f(-2.0f, -2.0f, 0.0f);
+	glVertex3f(2.0f, 0.0f, 0.0);
+	glVertex3f(0.0f, 2.0f, 0.0);
+	glEnd();
+
+	angle += 0.1f;
+
+	glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(1024, 768);
+	glutCreateWindow("Tutorial 01");
+
+	// register callbacks
+	glutDisplayFunc(renderScene);
+
+	// Here is our new entry in the main function
+	glutReshapeFunc(changeSize);
+
+	// here is the idle func registration
+	glutIdleFunc(renderScene);
+
+	glutMainLoop();
+
+	return 0;
+}
